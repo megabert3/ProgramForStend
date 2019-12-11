d13ceb75959aa0da1e2a0541821949f9536f6899
megabert3 <bert1993@mail.ru>
2019-12-11 17:40:36 +0300

    Добавил image и css.

--- src/stend/view/TestView.java
+++ src/stend/view/TestView.java
@@ -11,8 +11,8 @@ public class TestView extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainFrame.fxml"));
-        primaryStage.setTitle("НЕВА");
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setTitle("НЕВА СТЕНД");
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
 }
